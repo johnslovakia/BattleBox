f2e0a6d917587278ba9fe6e7fa037cb59aef8578
johnslovakia <johnslovakia06@gmail.com>
2024-11-13 13:47:41 +0100

    Initial commit

---
+++ src/main/java/cz/johnslovakia/battlebox/BattleBox.java
@@ -0,0 +1,17 @@
+package cz.johnslovakia.battlebox;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BattleBox extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
